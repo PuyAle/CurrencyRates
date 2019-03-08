d0f3083f6c565a941868bef0bd7e5118b9106dd5
root <root@DESKTOP-R94IB3A.localdomain>
2019-03-08 16:12:19 +0100

    changed the comparemethod for rates latest/historical. Dosen't work yet.

--- src/java/currencylogics/CurrencyController.java
+++ src/java/currencylogics/CurrencyController.java
@@ -1,24 +1,63 @@
 package currencylogics;
 
-import currencydao.CurrencyDao;
 import currencyentity.Currency;
 import currencypojo.CurrencyShell;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class CurrencyController {
 
-    CurrencyDao cd = new CurrencyDao();
+    private List<Currency> latestCurrencyList = new ArrayList<>();
+    private List<Currency> historicalCurrencyList = new ArrayList<>();
 
-    public void createCurrency(CurrencyShell cs) {
-
-        for (Map.Entry<String, Double> entry : cs.getRates().entrySet()) {
-            Currency c = new Currency(entry.getKey(), entry.getValue());
-            cd.addCurrency(c);
-
-        }
+    public List<Currency> getLatestCurrencyList() {
+        return latestCurrencyList;
     }
 
-//    public Currency getCurrencyByName(String s) {
-//
-//    }
+    public void setLatestCurrencyList(List<Currency> latestCurrencyList) {
+        this.latestCurrencyList = latestCurrencyList;
+    }
+
+    public List<Currency> getHistoricalCurrencyList() {
+        return historicalCurrencyList;
+    }
+
+    public void setHistoricalCurrencyList(List<Currency> historicalCurrencyList) {
+        this.historicalCurrencyList = historicalCurrencyList;
+    }
+//the if-statement dosen't work.
+
+    public void createCurrency(CurrencyShell cs) {
+        if (cs.getDate().equals(LocalDate.now().toString())) {
+            for (Map.Entry<String, Double> entry : cs.getRates().entrySet()) {
+                Currency c = new Currency(entry.getKey(), entry.getValue());
+                latestCurrencyList.add(c);
+            }
+        } else {
+            for (Map.Entry<String, Double> entry : cs.getRates().entrySet()) {
+                Currency c = new Currency(entry.getKey(), entry.getValue());
+                historicalCurrencyList.add(c);
+            }
+        }
+    }
+//fix this.
+
+    public void compareLatestHistoricalCurrency() {
+
+        //latestCurrencyList.stream().map()
+        for (Currency currency : latestCurrencyList) {
+            for (Currency currency1 : historicalCurrencyList) {
+                if (currency.getName().equals(currency1.getName())) {
+                    double i = currency.getRate() - currency1.getRate();
+                    System.out.println(currency.getName() + " " + i + "diffrence in rate\n");
+                } else {
+                    System.out.println("FAIL");
+                }
+            }
+        }
+
+    }
+
 }
--- src/java/ui/Menu.java
+++ src/java/ui/Menu.java
@@ -17,7 +17,7 @@ public class Menu {
             System.out.println("2. Print a list for over 300 currencies that are compared with Euro during a specifik time in history ");
             System.out.println("0. Exit");
             System.out.println("Enter your choice:\n");
-
+            CurrencyController cc = new CurrencyController();
             RestClient rc = new RestClient();
             int input = sc.nextInt();
             sc.nextLine();
@@ -26,14 +26,16 @@ public class Menu {
                     System.out.println(rc.client("http://data.fixer.io/api/latest?access_key=93c83c2929256213aba41d6809f95038"));
                     break;
                 case 2:
+                    //get list dosen't work
                     System.out.println("Enter the date you whant to see by writing the date in format YYYY-MM-DD");
                     String s = sc.nextLine();
-                    rc.client("http://data.fixer.io/api/" + s + "?access_key=myAPIKey");
+                    rc.client("http://data.fixer.io/api/" + s + "?access_key=93c83c2929256213aba41d6809f95038");
+                    cc.getHistoricalCurrencyList().forEach(System.out::println);
                     break;
                 case 3:
-                    CurrencyController cc = new CurrencyController();
-                    cc.createCurrency(rc.client("http://data.fixer.io/api/latest?access_key=93c83c2929256213aba41d6809f95038"));
 
+                    cc.createCurrency(rc.client("http://data.fixer.io/api/latest?access_key=93c83c2929256213aba41d6809f95038"));
+                    cc.compareLatestHistoricalCurrency();
                 case 0:
                     loop = false;
                     break;
